97c3e0a40e3d55f42838ee4ea68b959b2e5f7a92
Andrew Alferman <alfermaa@oregonstate.edu>
2017-08-03 15:32:32 -0700

    Got data on the number of function calls.

--- A_Posteriori_Stiffness_Detection.py
+++ A_Posteriori_Stiffness_Detection.py
@@ -323,7 +323,7 @@ savedata = 1
 equation = 'Autoignition'
 # Possible options are 'Stiffness_Index', 'Stiffness_Indicator', 'CEMA',
 # 'Stiffness_Ratio'
-method = 'Stiffness_Index'
+method = 'Stiffness_Indicator'
 # Make this true if you want to obtain the reference timescale of the stiffness
 # indicator.
 findtimescale = False
