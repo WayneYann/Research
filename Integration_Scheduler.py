fb3f213fa8b46e30a2a124bae28306ed326e424e
Andrew Alferman <alfermaa@oregonstate.edu>
2017-08-07 09:38:16 -0700

    Running implicit.

--- Integration_Scheduler.py
+++ Integration_Scheduler.py
@@ -333,8 +333,8 @@ tstop = 0.2
 abserr = 1.0e-17
 relerr = 1.0e-15
 # Switching thresholds
-indicatorthreshold = -1e99
-tempthreshold = 30000
+indicatorthreshold = 0
+tempthreshold = 0
 # Keep this at false, something isn't working with using the jacobian yet.
 usejac = False
 # Decide if you want to give pyJac N2 or not.
@@ -532,10 +532,10 @@ if len(solution) != len(tlist):
     print('tlist shape: {}'.format(np.shape(tlist)))
 
 if savedata:
-    solfilename = equation + '_Solution_Explicit_' + str(dt)
-    inttimingfilename = equation + '_Int_Times_Explicit_' + str(dt) + '_' +\
+    solfilename = equation + '_Solution_Implicit_' + str(dt)
+    inttimingfilename = equation + '_Int_Times_Implicit_' + str(dt) + '_' +\
         timer.strftime("%m_%d")
-    workfilename = equation + '_FunctionWork_Explicit_' + str(dt)
+    workfilename = equation + '_FunctionWork_Implicit_' + str(dt)
     if PaSR:
         inttimingfilename = 'PaSR_' + inttimingfilename
         workfilename = 'PaSR_' + workfilename
