96caa375697eae7a848e25080d2a1a17e7ddd01e
Andrew Alferman <alfermaa@oregonstate.edu>
2017-08-14 21:32:03 -0700

    Reran all the stiffness metrics at the new particle.

--- A_Posteriori_Stiffness_Detection.py
+++ A_Posteriori_Stiffness_Detection.py
@@ -323,7 +323,7 @@ savedata = True
 equation = 'Autoignition'
 # Possible options are 'Stiffness_Index', 'Stiffness_Indicator', 'CEMA',
 # 'Stiffness_Ratio'
-method = 'Stiffness_Indicator'
+method = 'Stiffness_Ratio'
 # Make this true if you want to obtain the reference timescale of the stiffness
 # indicator.
 findtimescale = False
