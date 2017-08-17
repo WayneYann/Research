91901d5c10528d84ba48c7e4dcfbf92a6d2f4c7f
Andrew Alferman <alfermaa@oregonstate.edu>
2017-08-17 15:22:51 -0700

    Made folder for GRI mech 3.

---
+++ GRI_Mech_3/gri-mech-3-files/jacobs/jacob_0.h
@@ -0,0 +1,8 @@
+#ifndef JACOB_HEAD_0
+#define JACOB_HEAD_0
+
+#include "../header.h"
+
+void eval_jacob_0 (const double, const double*, const double*, const double*, const double*, const double*, const double, const double, const double, const double*, const double, double*);
+
+#endif
