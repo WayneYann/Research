ad8a1dd1785548a787b1f8965326ff394140fcd8
Andrew Alferman <alfermaa@oregonstate.edu>
2017-08-30 13:58:49 -0700

    Made a few fixes to the integrator files.

--- GRI_Mech_3/Integration_PaSR_GRI_2.py
+++ GRI_Mech_3/Integration_PaSR_GRI_2.py
@@ -461,7 +461,9 @@ for particle in particlelist:
     halfflag = False
     timetwo = 0
     while solver.t < tstop:
+        functioncalls = 0
         stepstaken = 0
+        timetwo = 0.
         # Integrate until hitting the next tstep
         while solver.t < tnext:
             # Do this to force it to stop at every dt
@@ -503,16 +505,15 @@ for particle in particlelist:
                 solver.set_f_params(RHSparam)
                 solver.set_jac_params(RHSparam)
                 solver._integrator.iwork[2] = -1
-            # Initialize global variable for counting RHS function calls
-            functioncalls = 0
             time0 = timer.time()
             solver.integrate(tnext, step=True)
             time1 = timer.time()
-            if prevtime >= (tstart + 2*dt) and prevtime < (tstart + 3*dt):
+            if prevtime >= (tstart + dt) and prevtime < (tstart + 2*dt):
                 timetwo += time1 - time0
+                tempfcalls = functioncalls
+                stepstaken += 1
             # if solver.t <= prevtime:
             #     raise Exception('Error: Simulation not advancing!')
-            stepstaken += 1
             # Save the solution
             if solver.t >= tnext:
                 solution.append(solver.y)
@@ -570,7 +571,6 @@ for particle in particlelist:
                                               RHSparam
                                               )
                         tempfuncwork = functioncalls
-                        tstepsneeded.append(stepstaken)
                         # print('Halfway value: {}'.format(tstart + 2*dt))
                         # print('Current value: {}'.format(solver.t))
                         halfflag = True
@@ -592,12 +592,15 @@ for particle in particlelist:
                             indexvals.append(stiffindices[2])
                             indicatorvals.append(indicator)
                             CEMAvals.append(chemexmode)
-                            inttimes.append(timetwo)
-                        # Save the functionwork
+                            # Save the functionwork
                         if failflag:
                             functionwork.append(-1)
+                            inttimes.append(-1)
+                            tstepsneeded.append(-1)
                         else:
                             functionwork.append(tempfuncwork)
+                            inttimes.append(timetwo)
+                            tstepsneeded.append(stepstaken)
                 # Part of the code for non-PaSR.  Non-functional currently.
                 else:
                     solution.append(solver.y)
