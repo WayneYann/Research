58ed3bff79aeffc102a6ff6ce43ee42f5d828861
Andrew Alferman <alfermaa@oregonstate.edu>
2018-01-19 12:42:58 -0800

    ...

--- CSP/RKC.c
+++ CSP/RKC.c
@@ -4,14 +4,14 @@
 //
 // based on Sommeijer, Shampine, Verwer RKC.f (http://www.netlib.org/ode/rkc.f)
 
-#include "test.h"
+//#include "test.h"
 
 
 void RKC (Real t, Real* Y0, Real h, Real* Yj) {
   // performs one step using RKC
-  
+
   const usint s = 5;
-  
+
   const Real w0 = 1.0 + (2.0 / (13.0 * s * s));
   const Real temp1 = (w0 * w0) - 1.0;
 
@@ -20,8 +20,8 @@ void RKC (Real t, Real* Y0, Real h, Real* Yj) {
 
   const Real sinharg = sinh(arg);
   const Real w1 = sinharg * temp1 / ( cosh(arg) * s * temp2 - w0 * sinharg );
-  
-  
+
+
   Real bj = 1.0 / (4.0 * w0 * w0);
   Real bjm1 = bj, bjm2 = bj;
   Real mu1t = bjm1 * w1;
@@ -29,25 +29,25 @@ void RKC (Real t, Real* Y0, Real h, Real* Yj) {
   Real nuj = -1.0;
   Real mujt = 2.0 * w1;
   Real ajm1;
-  
+
   Real F0[NN], Y1[NN], Yjm2[NN], Yjm1[NN];
-  
+
   dydt (t, Y0, F0 );
-  
+
   for (usint i = 0; i < NN; ++i) {
     Y1[i] = Y0[i] + ( mu1t * h * F0[i] );
-    
+
     // set Yjm2 and Yjm1
     Yjm2[i] = Y0[i];
     Yjm1[i] = Y1[i];
   }
-  
+
   // use Yj to store derivative array
   dydt ( t + mujt * h, Y1, Yj );
 
   bjm2 = bjm1;
   bjm1 = bj;
-  
+
   // recurrence relations for Chebyshev polynomials
   Real Tj, dTj, d2Tj;
   Real Tjm1 = w0;
@@ -59,9 +59,9 @@ void RKC (Real t, Real* Y0, Real h, Real* Yj) {
   Real cj;
   Real cjm2 = 0.0;
   Real cjm1 = mujt;
-  
+
   for (usint j = 2; j <= s; ++j)
-  { 
+  {
     Tj = 2.0 * w0 * Tjm1 - Tjm2;
     dTj = 2.0 * w0 * dTjm1 - dTjm2 + 2.0 * Tjm1;
     d2Tj = 2.0 * w0 * d2Tjm1 - d2Tjm2 + 4.0 * dTjm1;
@@ -70,18 +70,18 @@ void RKC (Real t, Real* Y0, Real h, Real* Yj) {
     muj = 2.0 * w0 * bj / bjm1;
     nuj = - bj / bjm2;
     mujt = muj * w1 / w0;
-    
+
     // use Yj to store derivative array
     dydt ( t + cjm1 * h, Yjm1, Yj );
-    
+
     for (usint i = 0; i < NN; ++i) {
       Yj[i] = (1.0 - muj - nuj) * Y0[i] + (muj * Yjm1[i]) + (nuj * Yjm2[i]) + h * mujt * (Yj[i] - ajm1 * F0[i]);
-      
+
       Yjm2[i] = Yjm1[i];
       Yjm1[i] = Yj[i];
     }
     cj = muj * cjm1 + nuj * cjm2 + mujt * (1.0 - ajm1);
-    
+
     Tjm2 = Tjm1;
     Tjm1 = Tj;
     dTjm2 = dTjm1;
@@ -92,9 +92,9 @@ void RKC (Real t, Real* Y0, Real h, Real* Yj) {
     bjm1 = bj;
     cjm2 = cjm1;
     cjm1 = cj;
-    
+
   } // end loop over j
-  
+
   // the final Yj is returned
 
 }
